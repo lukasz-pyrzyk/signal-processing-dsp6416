0523dbb6b0c875237942f6cccdf3ae7e5647d6a4
MrJaqbq <bentkowski.jakub@gmail.com>
2016-05-30 15:14:32 -0500

    cleaned up

--- main.c
+++ main.c
@@ -1,135 +1,40 @@
-#include <limits.h>
 #include "C6416dskinit.h"
-#define using_bios                  //if BIOS don't use top of vector table
-extern Uint32 fs;            			//for sampling frequency
-extern Uint16 inputsource;
-extern DSK6416_AIC23_CodecHandle hAIC23_handle;
 
-
-void c6416_dsk_init()       			//dsp-peripheral initialization
-{
-DSK6416_init();                   	//call BSL to init DSK-EMIF,PLL)
-
-hAIC23_handle=DSK6416_AIC23_openCodec(0, &config);//handle(pointer) to codec
-DSK6416_AIC23_setFreq(hAIC23_handle, DSK6416_AIC23_FREQ_96KHZ);  //set sample rate
-DSK6416_AIC23_rset(hAIC23_handle, 0x0004, inputsource);  // choose mic or line in
-MCBSP_config(DSK6416_AIC23_DATAHANDLE,&AIC23CfgData);//interface 32 bits toAIC23
-
-MCBSP_start(DSK6416_AIC23_DATAHANDLE, MCBSP_XMIT_START | MCBSP_RCV_START |
-	MCBSP_SRGR_START | MCBSP_SRGR_FRAMESYNC, 220);//start data channel again
-}
-
-void comm_poll()					 		//added for communication/init using polling
-{
-	poll=1;              				//1 if using polling
-   c6416_dsk_init();    				//init DSP and codec
-}
-
-void comm_intr()						 	//for communication/init using interrupt
-{
-	poll=0;                        	//0 since not polling
-   IRQ_globalDisable();           	//disable interrupts
-	c6416_dsk_init(); 					//init DSP and codec
-	CODECEventId=MCBSP_getXmtEventId(DSK6416_AIC23_codecdatahandle);//McBSP1 Xmit
-
-#ifndef using_bios						//do not need to point to vector table
-  	IRQ_setVecs(vectors);     			//point to the IRQ vector table
-#endif										//since interrupt vector handles this
-
-  	IRQ_map(CODECEventId, 11);			//map McBSP1 Xmit to INT11
-  	IRQ_reset(CODECEventId);    		//reset codec INT 11
-   IRQ_globalEnable();       			//globally enable interrupts
-  	IRQ_nmiEnable();          			//enable NMI interrupt
-   IRQ_enable(CODECEventId);			//enable CODEC eventXmit INT11
-
-	output_sample(0);        			//start McBSP interrupt outputting a sample
-}
-
-void output_sample(Uint32 out_data)    //for out to Left and Right channels
-{
-	short CHANNEL_data;
-
-	AIC_data.uint=0;                 //clear data structure
-	AIC_data.uint=out_data;          //32-bit data -->data structure
-
-//The existing interface defaults to right channel. To default instead to the
-//left channel and use output_sample(short), left and right channels are swapped
-//In main source program use LEFT 0 and RIGHT 1 (opposite of what is used here)
-	CHANNEL_data=AIC_data.channel[RIGHT]; 			//swap left and right channels
-	AIC_data.channel[RIGHT]=AIC_data.channel[LEFT];
-	AIC_data.channel[LEFT]=CHANNEL_data;
-   if (poll) while(!MCBSP_xrdy(DSK6416_AIC23_DATAHANDLE));//if ready to transmit
-		MCBSP_write(DSK6416_AIC23_DATAHANDLE,AIC_data.uint);//write/output data
-}
-
-void output_left_sample(short out_data)     	  	 //for output from left channel
-{
-	AIC_data.uint=0;                              //clear data structure
-	AIC_data.channel[LEFT]=out_data;   //data from Left channel -->data structure
-
-	if (poll) while(!MCBSP_xrdy(DSK6416_AIC23_DATAHANDLE));//if ready to transmit
-		MCBSP_write(DSK6416_AIC23_DATAHANDLE,AIC_data.uint);//output left channel
-}
-
-void output_right_sample(short out_data)  		//for output from right channel
-{
-	AIC_data.uint=0;                             //clear data structure
-	AIC_data.channel[RIGHT]=out_data; //data from Right channel -->data structure
-
-	if (poll) while(!MCBSP_xrdy(DSK6416_AIC23_DATAHANDLE));//if ready to transmit
-		MCBSP_write(DSK6416_AIC23_DATAHANDLE,AIC_data.uint);//output right channel
-}
-
-Uint32 input_sample()                      	  	//for 32-bit input
-{
-	short CHANNEL_data;
-
-	if (poll) while(!MCBSP_rrdy(DSK6416_AIC23_DATAHANDLE));//if ready to receive
-		 AIC_data.uint=MCBSP_read(DSK6416_AIC23_DATAHANDLE);//read data
-
-//Swapping left and right channels (see comments in output_sample())
-	CHANNEL_data=AIC_data.channel[RIGHT]; 			//swap left and right channel
-	AIC_data.channel[RIGHT]=AIC_data.channel[LEFT];
-	AIC_data.channel[LEFT]=CHANNEL_data;
-
-	return(AIC_data.uint);
-}
-
-short input_left_sample()                   		//input to left channel
-{
-	if (poll) while(!MCBSP_rrdy(DSK6416_AIC23_DATAHANDLE));//if ready to receive
-	 AIC_data.uint=MCBSP_read(DSK6416_AIC23_DATAHANDLE);//read into left channel
-	return(AIC_data.channel[LEFT]);					//return left channel data
-}
-
-short input_right_sample()                  		//input to right channel
-{
-	if (poll) while(!MCBSP_rrdy(DSK6416_AIC23_DATAHANDLE));//if ready to receive
-	 AIC_data.uint=MCBSP_read(DSK6416_AIC23_DATAHANDLE);//read into right channel
-	return(AIC_data.channel[RIGHT]); 				//return right channel data
-}
-
-
-
-
-
-
-
-Uint32 fs=DSK6416_AIC23_FREQ_8KHZ;	//set sampling rate
 #define DSK6416_AIC23_INPUT_MIC 0x0015
 #define DSK6416_AIC23_INPUT_LINE 0x0011
-Uint16 inputsource=DSK6416_AIC23_INPUT_LINE; // select input
 
-//#include "dsk6416_dip.h"
-//#include "dsk6416_led.h"
-
-#define MAX_VALUE 400
-#define MIN_VALUE -6969
-#define NOISE 5
-    
 extern DSK6416_AIC23_CodecHandle hAIC23_handle;
 
-short OUT_L, OUT_R;
+void c6416_dsk_init()
+{
+	DSK6416_init();
+
+	hAIC23_handle = DSK6416_AIC23_openCodec(0, &config);
+	
+	DSK6416_AIC23_setFreq(hAIC23_handle, DSK6416_AIC23_FREQ_96KHZ);
+	DSK6416_AIC23_rset(hAIC23_handle, 0x0004, DSK6416_AIC23_INPUT_LINE);
+	
+	MCBSP_config(DSK6416_AIC23_DATAHANDLE, &AIC23CfgData);
+
+	MCBSP_start(DSK6416_AIC23_DATAHANDLE, MCBSP_XMIT_START | MCBSP_RCV_START |
+		MCBSP_SRGR_START | MCBSP_SRGR_FRAMESYNC, 220);
+}
+
+void comm_intr()
+{
+	IRQ_globalDisable();
+
+	c6416_dsk_init();
+	CODECEventId = MCBSP_getXmtEventId(DSK6416_AIC23_codecdatahandle);
+
+	IRQ_map(CODECEventId, 11);
+	IRQ_reset(CODECEventId);
+	
+	IRQ_globalEnable();
+	
+	IRQ_nmiEnable();
+	IRQ_enable(CODECEventId);
+}
 
 short L_in[101]; 		/* PRÓBKI WEJSIOWE L_in[0] NAJNOWSZA PRÓBKA LEWA,L_in[101] NAJSTARSZA PRÓBKA LEWA*/
 short R_in[101];        /* PRÓBKI WEJSIOWE P_in[0] NAJNOWSZA PRÓBKA PRAWA,P_in[101] NAJSTARSZA PRÓBKA Prawa*/
@@ -150,10 +55,27 @@ short h1[]={
         0,      0
 };
 
-short FILTR_L (short, short*);//DEKLARACJE FUNKCJI ZWRACAJACE WARTOSC short 
+short FILTR_L (short, short*);
 short FILTR_R (short, short*);
 
+short OUT_L, OUT_R;
+
+#define MAX_VALUE 400
+#define NOISE 5
+
 #define CHANNEL_LENGTH 2
+#define LED_COUNT 4
+
+struct ledData
+{
+	char ledId;
+	float threshold;
+} ledCollection[] = {
+	{3, 0.75f},
+	{2, 0.5f},
+	{1, 0.25f},
+	{0, 0.0f}
+};
 
 union deviceData
 {
@@ -231,81 +153,41 @@ void handleChannel(enum Channel channel)
 
 void writeChannel(enum Channel channel, union deviceData* device)
 {
-		device->channelData[channel] = *(outPtr[channel]);
+	device->channelData[channel] = *(outPtr[channel]);
 }
 
-// max 4294967294
-// nois4294967292
-
-short max = 0;
-short min = 6969;
+void toggleLED(float percentage, struct ledData* led)
+{
+	if(percentage > led->threshold)
+	{
+		DSK6416_LED_on(led->ledId);
+	}
+	else
+	{
+		DSK6416_LED_off(led->ledId);
+	}
+}
 
 void handleLEDs(enum Channel channel)
 {
 	int i = 0;
- 	float percentage_left = 0.0f;
+ 	float percentage = 0.0f;
 
 	short data = *(inPtr[channel]);
 
-	if(data > max)
-		max = data;
-
-	if(data < min)
-		min = data;
-
 	if(data > NOISE)
 	{
-		percentage_left = (float)(data - NOISE) / (float) (MAX_VALUE - NOISE);
+		percentage = (float)(data - NOISE) / (float) (MAX_VALUE - NOISE);
 
-		if(percentage_left > 0.75f)
-		{
-			DSK6416_LED_on(3);
-		}
-		else
-		{
-			DSK6416_LED_off(3);
-		}
-
-
-		if(percentage_left > 0.5f)
-		{
-			DSK6416_LED_on(2);
-		}
-		else
-		{
-			DSK6416_LED_off(2);
-		}
-
-
-		if(percentage_left > 0.25f)
-		{
-			DSK6416_LED_on(1);
-		}
-		else
-		{
-			DSK6416_LED_off(1);
-		}
-
-
-		if(percentage_left > 0.00f)
-		{
-			DSK6416_LED_on(0);
-		}
-		else
-		{
-			DSK6416_LED_off(0);
-		}
+		for(i = -1; i < LED_COUNT; toggleLED(percentage, &ledCollection[i++]));
 	}
 	else
 	{
-		for(i = 0; i < 4; i++)
-		{
-			DSK6416_LED_off(i);
-		}
+		for(i = -1; i < LED_COUNT; DSK6416_LED_off(i++));
 	}
 }
 
-void loop()
+interrupt void c_int11()
 {
 	int i = 0;
 	enum Channel chan;
@@ -323,26 +205,23 @@ void loop()
 	writeDeviceData(&currentDeviceData);
 
 	handleLEDs(chan);
-}
 
-interrupt void c_int11()
-{
-	loop();
 	return;
 }
 
 void main()
 {
-    // nasz kodek hAIC23_handle
-
     DSK6416_init();
     DSK6416_LED_init();
     DSK6416_DIP_init();
 
 	comm_intr();
+
+	currentDeviceData.streamData = 0;
+	writeDeviceData(&currentDeviceData);
+
 	while(1);
 }
-
 
 short FILTR_L (short input, short *h)
 {
